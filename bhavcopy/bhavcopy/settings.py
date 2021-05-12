2a3a288830c1cf38ec5fbce77f24629f350b5c07
amoghdatt <amoghdatt@gmail.com>
2021-05-12 07:06:03 +0000

    fix static files render

--- bhavcopy/bhavcopy/settings.py
+++ bhavcopy/bhavcopy/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 from celery.schedules import crontab
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -127,7 +128,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = 'var/static_root/'
-STATICFILES_DIRS = ['static']
+STATICFILES_DIRS = [f"{BASE_DIR}/static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- bhavcopy/bhavcopy/urls.py
+++ bhavcopy/bhavcopy/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.decorators.csrf import csrf_exempt
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('equity/', include('equity.urls')),
     path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
-] 
+]
