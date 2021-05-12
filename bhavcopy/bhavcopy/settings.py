48bc47af6574e398268cca9a508bde363981dd3e
amoghdatt <amoghdatt@gmail.com>
2021-05-12 04:02:56 +0000

    remove secret key

--- bhavcopy/bhavcopy/settings.py
+++ bhavcopy/bhavcopy/settings.py
@@ -20,9 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^_ilhd_ffx3zozw!@g6ro9mj4awxb$&l9-3u=!x=5zi1hj(kzl'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
